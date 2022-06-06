d3e2746e5b4e7a0a5152d137e2f37e204ec12654
Emma Lichtenstein <emma.lichtenstein@kellogg.northwestern.edu>
2022-06-06 13:42:16 +0000

    user auth - password and controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,21 +2,29 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @current_user = User.find_by({ "id" => session["user_id"] })
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({ "place_id" => @place["id"] })
+    if @current_user
+      @posts = Post.where({ "place_id" => @place["id"] ,"user_id" => @current_user["id"]})
   end
+end
 
   def new
     @place = Place.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
+  if @current_user
+      @place = Place.new
+      @place["name"] = params["place"]["name"]
+      @place.save
+  else
+    flash["notice"] = "Sign in to add new places."
+  end
     redirect_to "/places"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+ 
+  def show
+    @user = User.find_by({"id" => params["id"]})
+  end
 
   def create
     @user = User.new
+    @user["username"] = params["user"]["username"]
     @user["first_name"] = params["user"]["first_name"]
     @user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
     @user["password"] = params["user"]["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/login"
   end
 end
